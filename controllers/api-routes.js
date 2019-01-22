de784bf3ef61872015173e899a1dbbb6792009be
Kevin519 <kpconway519@gmail.com>
2019-01-21 21:00:55 -0500

    fixed the api routes

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -26,7 +26,7 @@ module.exports = function (app) {
       last_name: barber.lastName,
       location: barber.location
     });
-
+  });
     // NOT DONE YET
 
   app.post("/customer/new", function (req, res) {
@@ -38,7 +38,7 @@ module.exports = function (app) {
       last_name: customer.lastName,
       location: customer.location
     });
-
+  });
         // NOT DONE YET
 
   app.post("/service/new", function (req, res) {
@@ -51,24 +51,7 @@ module.exports = function (app) {
       price: service.price,
       duration: service.duration
     });
+  });
 
-
-        //Don't really know what the question mark is all about here. I took this from the star wars thing.
-
-        // app.get("/barber/:name?", async function(req, res) {
-        //   if (req.params.characters) {
-        //     // Display the JSON for ONLY that character.
-        //     // (Note how we're using the ORM here to run our searches)
-        //     let result = await Character.findOne({
-        //                   where: {routeName: req.params.characters}})
-        //       let credentials = await credentials.find(result)
-        //       let certification = await AMA.find(credentials)
-        //   } else {
-        //     let result = await Character.findAll()
-        //                   return res.json(result);
-        //   }
-        // });
-
-        res.status(204).end();
-      });
-    };
+res.status(204).end();
+};
