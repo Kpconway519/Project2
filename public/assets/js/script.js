9c826e968944ba850b5c66b454be0e4fdb1263b7
Austin <a.rossfortson@gmail.com>
2019-01-24 19:16:55 -0500

    Set up the link for CSS and JS in the Assets folder properly

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,6 +3,7 @@
 
 // Code here handles what happens when a user submits a new character on the form.
 // Effectively it takes the form inputs then sends it to the server to save in the DB.
+console.log("hello")
 
 $("#barber-add-btn").on("click", function(event) {
     event.preventDefault();
