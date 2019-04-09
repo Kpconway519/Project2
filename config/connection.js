e941d1397b9602b3c7edc20d9b03893088830ed4
Kpconway519 <kpconway519@gmail.com>
2019-04-09 13:28:33 -0400

    pushing new connection file

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("barberoo", "root", process.env.password, {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+let sequelize;
 
+if (process.env.JAWSDB_URL) {
+   sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else{
+  sequelize = new Sequelize("barberoo", "root", process.env.password, {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+}
+  
 module.exports = sequelize;
